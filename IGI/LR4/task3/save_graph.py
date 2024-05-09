94cfacd0a7f9a2fc5fb336e4171615aad89a7e91
Kirill <gimpelcomp@gmail.com>
2024-05-09 11:20:45 +0300

    Changed line thickness

--- IGI/LR4/task3/save_graph.py
+++ IGI/LR4/task3/save_graph.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 
 
 def save_graph(path, x1, y1, x2, y2):
-	plt.plot(x1, y1, label='math.exp', color="r")
-	plt.plot(x2, y2, label='taylor_exp', color="g")
+	plt.plot(x1, y1, label='math.exp', linewidth=7, color="r")
+	plt.plot(x2, y2, label='taylor_exp', linewidth=2, color="g")
 	plt.axis('equal')
 	plt.legend()
 	plt.savefig(path)
